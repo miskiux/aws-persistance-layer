a9117da6b257f084bfac30875581e697ebd54254
justas <j.gecas5@gmail.com>
2024-02-01 21:18:02 +0200

    attach efs utils role for ec2

--- terraform/layers/persistance-layer/iam/main.tf
+++ terraform/layers/persistance-layer/iam/main.tf
@@ -1,88 +1,34 @@
-resource "aws_iam_instance_profile" "efs_utils_profile" {
-  name = "efs_utils_profile"
+resource "aws_iam_instance_profile" "efs_utils_role_profile" {
+  name = "efs_utils_role_profile"
   role = aws_iam_role.efs_utils_role.name
-
-  tags = {
-    Name = "efs_utils_iam_profile_instance"
-  }
 }
+
+resource "aws_iam_role_policy_attachment" "efs-system-utils-role-attach" {
+  role       = aws_iam_role.efs_utils_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonElasticFileSystemsUtils"
+}
+
 
 resource "aws_iam_role" "efs_utils_role" {
   name        = "AmazonElasticFileSystemsUtilsRole"
   description = "Allows customers to use AWS Systems Manager to automatically manage Amazon EFS utilities (amazon-efs-utils) package on their EC2 instances, and use CloudWatchLog to get EFS file system mount success/failure notifications."
 
   assume_role_policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
+    Version = "2012-10-17"
+    Statement = [
       {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssm:DescribeAssociation",
-          "ssm:GetDeployablePatchSnapshotForInstance",
-          "ssm:GetDocument",
-          "ssm:DescribeDocument",
-          "ssm:GetManifest",
-          "ssm:GetParameter",
-          "ssm:GetParameters",
-          "ssm:ListAssociations",
-          "ssm:ListInstanceAssociations",
-          "ssm:PutInventory",
-          "ssm:PutComplianceItems",
-          "ssm:PutConfigurePackageResult",
-          "ssm:UpdateAssociationStatus",
-          "ssm:UpdateInstanceAssociationStatus",
-          "ssm:UpdateInstanceInformation"
-        ],
-        "Resource" : "*"
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ssmmessages:CreateControlChannel",
-          "ssmmessages:CreateDataChannel",
-          "ssmmessages:OpenControlChannel",
-          "ssmmessages:OpenDataChannel"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2messages:AcknowledgeMessage",
-          "ec2messages:DeleteMessage",
-          "ec2messages:FailMessage",
-          "ec2messages:GetEndpoint",
-          "ec2messages:GetMessages",
-          "ec2messages:SendReply"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "elasticfilesystem:DescribeMountTargets"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "ec2:DescribeAvailabilityZones"
-        ],
-        "Resource" : "*"
-      },
-      {
-        "Effect" : "Allow",
-        "Action" : [
-          "logs:PutLogEvents",
-          "logs:DescribeLogStreams",
-          "logs:DescribeLogGroups",
-          "logs:CreateLogStream",
-          "logs:CreateLogGroup",
-          "logs:PutRetentionPolicy"
-        ],
-        "Resource" : "*"
-      }
     ]
-  })
+    }
+  )
+
+  tags = {
+    tag-key = "efs_utils_role"
+  }
 }
--- terraform/layers/persistance-layer/iam/outputs.tf
+++ terraform/layers/persistance-layer/iam/outputs.tf
@@ -1,3 +1,3 @@
 output "iam_instance_profile" {
-  value = aws_iam_instance_profile.efs_utils_profile.name
+  value = aws_iam_instance_profile.efs_utils_role_profile.name
 }
